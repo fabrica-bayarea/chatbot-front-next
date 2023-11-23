cfde2e1ade6c8f7c9a79bbc4e5e1c4e8fbeb9fb0
Paulo Lima <limapaulobsb@gmail.com>
2023-11-23 15:54:40 -0300

    feat: handle passwords in json-server

--- server/middlewares/login.js
+++ server/middlewares/login.js
@@ -1,14 +1,17 @@
+const argon2 = require('argon2');
 const config = require('../../json-server.json');
 
 const URL = `http://localhost:${config.port}`;
 
 module.exports = async function ({ body, path }, res, next) {
   if (path === '/login') {
-    // Check if the user exists and if the password is valid
+    // Checks if the user exists and if the password is valid
     const data = await fetch(`${URL}/users?email=${body.email}`);
     const [user] = await data.json();
 
-    if (user && user.password === body.password) {
+    if (user && (await argon2.verify(user.password, body.password))) {
+      delete user.password;
+
       return res.status(200).json({ user });
     }
 
