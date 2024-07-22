1b69aba88c362c1ae48f78a169201d0d30105a83
Paulo Lima <limapaulobsb@gmail.com>
2024-07-22 16:00:43 -0300

    feat: update support email

--- src/components/EmailTemplates/SupportUpdateEmail/index.tsx
+++ src/components/EmailTemplates/SupportUpdateEmail/index.tsx
@@ -1,5 +1,7 @@
 import {
   Body,
+  Button,
+  Column,
   Container,
   Head,
   Heading,
@@ -8,6 +10,7 @@ import {
   Img,
   Link,
   Preview,
+  Row,
   Section,
   Text,
 } from '@react-email/components';
@@ -16,7 +19,9 @@ import * as React from 'react';
 
 import { Message, Profile } from '@/utils/definitions';
 
-export const SupportUpdateEmail = ({
+const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '';
+
+function SupportUpdateEmail({
   id,
   collaboratorProfile,
   ownerProfile,
@@ -26,154 +31,189 @@ export const SupportUpdateEmail = ({
   collaboratorProfile: Profile;
   ownerProfile: Profile;
   messages: Message[];
-}) => (
-  <Html>
-    <Head />
-    <Preview>
-      {collaboratorProfile.name} lhe enviou uma atualização da conversa ID: {id}
-    </Preview>
-    <Body style={main}>
-      <Container style={container}>
-        <Heading style={heading}>Olá, {ownerProfile.name}! 👋</Heading>
-        <Section>
-          <Img
-            src={collaboratorProfile.picture}
-            height={'80'}
-            width={'auto'}
-            style={{
-              borderRadius: '50%',
-              filter: 'grayscale(50%)',
-              margin: '40px auto 20px',
-            }}
-          />
-          <Text style={introductionText}>
-            {collaboratorProfile.name} lhe enviou uma atualização de conversa.
-          </Text>
-        </Section>
-        <Section style={conversation}>
-          {messages.map(({ content, role }, i) => (
-            <Text key={i} style={messageFrom[role]}>
-              {content}
+}) {
+  const avatar: React.CSSProperties = {
+    backgroundImage: `url(${collaboratorProfile.picture})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    borderRadius: '50%',
+    filter: 'grayscale(50%)',
+    height: '80px',
+    margin: '40px 0 20px',
+    width: '80px',
+  };
+
+  const button: React.CSSProperties = {
+    backgroundColor: '#ed2241',
+    borderRadius: '4px',
+    color: 'white',
+    fontSize: '16px',
+    padding: '20px 40px',
+  };
+
+  const buttonContainer: React.CSSProperties = {
+    margin: '80px 0',
+  };
+
+  const closingText: React.CSSProperties = {
+    fontSize: '20px',
+  };
+
+  const container: React.CSSProperties = {
+    maxWidth: '600px',
+  };
+
+  const conversation: React.CSSProperties = {
+    backgroundColor: '#fff0f0',
+    margin: '40px 0 20px',
+    padding: '20px',
+  };
+
+  const divider: React.CSSProperties = {
+    borderColor: '#dddddd',
+    margin: '20px 0',
+  };
+
+  const footer: React.CSSProperties = {
+    padding: '0 20px',
+  };
+
+  const footerLink: React.CSSProperties = {
+    borderBottom: '1px solid #edaeda',
+    color: '#edaeda',
+    fontSize: '12px',
+    marginRight: '20px',
+  };
+
+  const footerText: React.CSSProperties = {
+    color: '#8898aa',
+    fontSize: '12px',
+  };
+
+  const heading: React.CSSProperties = {
+    fontSize: '28px',
+    marginTop: '40px',
+  };
+
+  const introductionText: React.CSSProperties = {
+    fontSize: '20px',
+  };
+
+  const logo = {
+    marginBottom: '20px',
+    opacity: 0.8,
+  };
+
+  const main: React.CSSProperties = {
+    backgroundColor: 'white',
+    fontFamily: 'Ubuntu,sans-serif',
+  };
+
+  const paragraph: React.CSSProperties = {
+    fontSize: '16px',
+    lineHeight: '26px',
+  };
+
+  const message: React.CSSProperties = {
+    ...paragraph,
+    margin: '30px 0',
+    whiteSpace: 'pre-line',
+  };
+
+  const serviceText = {
+    ...message,
+    borderRadius: '8px',
+    padding: '20px',
+  };
+
+  const messageFrom: {
+    user: React.CSSProperties;
+    assistant: React.CSSProperties;
+    collaborator: React.CSSProperties;
+  } = {
+    user: {
+      ...message,
+    },
+    assistant: {
+      ...serviceText,
+      backgroundColor: '#f0e4e4',
+    },
+    collaborator: {
+      ...serviceText,
+      backgroundColor: '#f0d4d4',
+    },
+  };
+
+  return (
+    <Html>
+      <Head />
+      <Preview>
+        {collaboratorProfile.name} lhe enviou uma atualização da conversa ID: {id}
+      </Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Heading style={heading}>Olá, {ownerProfile.name}! 👋</Heading>
+          <Section>
+            <Row>
+              <Column align="center">
+                <div style={avatar}></div>
+                <Text style={introductionText}>
+                  {collaboratorProfile.name} lhe enviou uma atualização de conversa.
+                </Text>
+              </Column>
+            </Row>
+          </Section>
+          <Section style={conversation}>
+            {messages.map(({ content, role }, i) => (
+              <Text key={i} style={messageFrom[role]}>
+                {content}
+              </Text>
+            ))}
+          </Section>
+          <Section>
+            <Text style={closingText}>Atenciosamente,</Text>
+            <Text style={closingText}>Equipe IESB</Text>
+          </Section>
+          <Section style={buttonContainer}>
+            <Row>
+              <Column align="center">
+                <Text style={paragraph}>
+                  Se estiver satisfeito, você pode encerrar este atendimento e fazer uma
+                  avaliação.
+                </Text>
+                <Button href={`${baseUrl}/`} style={button}>
+                  Encerrar atendimento
+                </Button>
+              </Column>
+            </Row>
+          </Section>
+          <Section style={footer}>
+            <Text style={footerText}>
+              Você está recebendo este e-mail porque possui um atendimento pendente em
+              nosso sistema.
             </Text>
-          ))}
-        </Section>
-        <Section>
-          <Text style={closingText}>Atenciosamente,</Text>
-          <Text style={closingText}>Equipe IESB</Text>
-        </Section>
-        <Section style={footer}>
-          <Text style={footerText}>
-            Você está recebendo este e-mail porque possui um atendimento pendente em nosso
-            sistema.
-          </Text>
-          <Link href="/" style={footerLink}>
-            Site
-          </Link>
-          <Link href="/" style={footerLink}>
-            Política de Privacidade
-          </Link>
-          <Hr style={divider} />
-          <Section></Section>
-          <Img
-            src={'https://utfs.io/f/3b0c82a2-c46e-4378-91c0-a19ee1e9f817-gn7svz.png'}
-            height={50}
-            width={'auto'}
-            style={{
-              margin: '20px 0',
-              opacity: 0.8,
-            }}
-          />
-          <Text style={footerText}>Centro Universitário IESB</Text>
-          <Text style={footerText}>
-            SGAS Quadra 613/614, Via L2 Sul - Asa Sul, Brasília - DF, 70200-730
-          </Text>
-        </Section>
-      </Container>
-    </Body>
-  </Html>
-);
+            <Link href={baseUrl} style={footerLink}>
+              Site
+            </Link>
+            <Link href="/" style={footerLink}>
+              Política de Privacidade
+            </Link>
+            <Hr style={divider} />
+            <Img
+              src={'https://utfs.io/f/3b0c82a2-c46e-4378-91c0-a19ee1e9f817-gn7svz.png'}
+              height={50}
+              width={50}
+              style={logo}
+            />
+            <Text style={footerText}>Centro Universitário IESB</Text>
+            <Text style={footerText}>
+              SGAS Quadra 613/614, Via L2 Sul - Asa Sul, Brasília - DF, 70200-730
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+}
 
 export default SupportUpdateEmail;
-
-const closingText = {
-  fontSize: '20px',
-};
-
-const container = {
-  maxWidth: '600px',
-};
-
-const conversation = {
-  backgroundColor: '#fff0f0',
-  margin: '40px 0 20px',
-  padding: '20px',
-};
-
-const divider = {
-  borderColor: '#dddddd',
-  marginTop: '20px',
-};
-
-const footer = {
-  marginTop: '80px',
-  padding: '0 20px',
-};
-
-const footerLink = {
-  color: '#edaeda',
-  fontSize: '12px',
-  marginRight: '20px',
-  borderBottom: '1px solid #edaeda',
-};
-
-const footerText = {
-  color: '#8898aa',
-  fontSize: '12px',
-};
-
-const heading = {
-  fontSize: '28px',
-  marginTop: '40px',
-};
-
-const introductionText = {
-  fontSize: '20px',
-  margin: '0 auto',
-  width: 'fit-content',
-};
-
-const main = {
-  backgroundColor: '#ffffff',
-  fontFamily: 'Ubuntu,sans-serif',
-};
-
-const paragraph = {
-  fontSize: '16px',
-  lineHeight: '26px',
-};
-
-const message = {
-  ...paragraph,
-  margin: '30px 0',
-  whiteSpace: 'pre-line',
-};
-
-const serviceText = {
-  ...message,
-  borderRadius: '8px',
-  padding: '20px',
-};
-
-const messageFrom = {
-  user: {
-    ...message,
-  },
-  assistant: {
-    ...serviceText,
-    backgroundColor: '#f0e4e4',
-  },
-  collaborator: {
-    ...serviceText,
-    backgroundColor: '#f0d4d4',
-  },
-};
